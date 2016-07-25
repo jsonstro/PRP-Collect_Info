ea4e648739a2cf6cdc7bda3e6b631ccc93396c2a
Josh Sonstroem <jsonstro@ucsc.edu>
2016-07-25 11:30:47 -0700

    Mods to the looping and added getopt arg support.

--- collect_info_csv.sh
+++ collect_info_csv.sh
@@ -7,61 +7,73 @@
 args=0
 count=0
 
-echo "\"" #--> Server and OS"
-uname -a
-echo "\", \"" #--> CPU Type"
-lscpu
-echo "\", \"" #--> IRQ Balance?"
-service irqbalance status
-echo "\", \"" #--> SYSCTL Settings?"
-sysctl -a | egrep 'wmem|rmem|netdev_max_backlog|cong|timest|somaxconn' | egrep -v 'min|lowmem' 
-echo "\", \"" #--> RC.local?"
-if [ -f /etc/rc.local ]; then
-	cat /etc/rc.local | grep -v \# | egrep -v ^$
-fi
-echo "\", \"" #--> What test suites are running?"
-netstat -anp | egrep '4823|5000|5001|5201' | awk '{print $7 " (" $4 ")" }' | cut -d/ -f2 | grep -v ^-
+while getopts 'v' flag; do
+    case "${flag}" in
+        v) args='1' ;;
+        *) error "Unexpected option ${flag}" ;;
+    esac
+done
 
 if [ "$#" -gt 0 ]; then
-   for iface in "$@"; do
-	count=$(expr $count + 1)
-	echo "\", \"--> Interface $count: $iface"
-	ifconfig $iface | egrep -v ^$
-	echo "\", \"" #Interface info for $iface?"
-	ethtool -i $iface
-	drv=$(ethtool -i $iface | grep driver | awk '{print $2}' | cut -d_ -f1)
-	if [[ "$drv" == *"mlx"* ]]; then
-		echo "\", \" * --> Detected a Mellanox at $iface!"
-		#echo "IRQ Affinity for $iface?"
-		show_irq_affinity.sh $iface
-		if [ "$args" -eq 1 ]; then
-			mlnx_tune
-		fi
-    else
-        echo "\", \" * --> Did not detect a Mellanox ($drv) at $iface!"
-	fi
-	echo "\", \"" #Interrupts for $iface?"
-	cat /proc/interrupts | head -1
-	drvs=$(cat /proc/interrupts | egrep "$drv" | head -$count | tail -1)
-	echo "$drvs"
-	cat /proc/interrupts | egrep "$iface"
-	echo "\", \""
-	ethtool -a $iface | grep -v $iface | egrep -v ^$
-	echo "\", \""
-	ethtool -c $iface | grep -v $iface | egrep -v ^$
-	echo "\", \""
-	ethtool -g $iface | grep -v $iface | egrep -v ^$
-	echo "\", \""
-	ethtool -k $iface | grep -v fixed | grep -v $iface | egrep -v ^$
-	echo "\", \""
-	ethtool --show-priv-flags $iface | grep -v $iface | egrep -v ^$
-#	echo "\","
-#	echo ""
+    echo "\"" #--> whoami"
+    whoami && date
+    echo "\", \"" #--> Server and OS"
+    uname -a
+    echo "\", \"" #--> CPU Type"
+    lscpu
+    echo "\", \"" #--> IRQ Balance?"
+    service irqbalance status
+    echo "\", \"" #--> SYSCTL Settings?"
+    sysctl -a | egrep 'wmem|rmem|netdev_max_backlog|cong|timest|somaxconn' | egrep -v 'min|lowmem' 
+    echo "\", \"" #--> RC.local?"
+    if [ -f /etc/rc.local ]; then
+    	cat /etc/rc.local | grep -v \# | egrep -v ^$
+    fi
+    echo "\", \"" #--> What test suites are running?"
+    netstat -anp | egrep '4823|5000|5001|5201' | awk '{print $7 " (" $4 ")" }' | cut -d/ -f2 | grep -v ^-
+
+    for iface in "$@"; do
+        count=$(expr $count + 1)
+        echo "\", \"--> Interface $count: $iface"
+       	ifconfig $iface | egrep -v ^$
+       	echo "\", \"" #Interface info for $iface?"
+        ethtool -i $iface
+        drv=$(ethtool -i $iface | grep driver | awk '{print $2}' | cut -d_ -f1)
+        if [[ "$drv" == *"mlx"* ]]; then
+            echo "\", \" * --> Detected a Mellanox at $iface!"
+            #echo "IRQ Affinity for $iface?"
+            show_irq_affinity.sh $iface
+            if [ "$args" -eq 1 ]; then
+       	        mlnx_tune
+            fi
+        else
+            echo "\", \" * --> Did not detect a Mellanox ($drv) at $iface!"
+	    fi
+        echo "\", \"" #Interrupts for $iface?"
+        cat /proc/interrupts | head -1
+        drvs=$(cat /proc/interrupts | egrep "$drv" | head -$count | tail -1)
+        echo "$drvs"
+        cat /proc/interrupts | egrep "$iface"
+        echo "\", \""
+        ethtool -a $iface | grep -v $iface | egrep -v ^$
+        echo "\", \""
+        ethtool -c $iface | grep -v $iface | egrep -v ^$
+        echo "\", \""
+        ethtool -g $iface | grep -v $iface | egrep -v ^$
+        echo "\", \""
+        ethtool -k $iface | grep -v fixed | grep -v $iface | egrep -v ^$
+        echo "\", \""
+        ethtool --show-priv-flags $iface | grep -v $iface | egrep -v ^$
+        #	echo "\","
+        #	echo ""
     done
-echo "\""
+    echo "\""
 else
-    echo "--> Choose from available interfaces?"
     ifconfig -a
     echo ""
-    echo "Usage: ./collect_info.sh IF1 IF2 ... IFn"
+    echo "--> Choose from the list of available interfaces above..."
+    echo ""
+    echo "Usage: ./collect_info_csv.sh [-v] IF1 IF2 ... IFn > outfile.csv "
+    echo "  -v = run the mlnx_tune script if a mellanox is detected at any interface"
+    echo ""
 fi
